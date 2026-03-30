a9c58ebf949ceed816828b40d7313b8711d515a7
PhosCity <phoscity2@gmail.com>
2026-03-30 17:26:25 +0545

    add service manager

---
+++ src/pacs/manager/service_manager.py
@@ -0,0 +1,161 @@
+import subprocess
+
+from rich.columns import Columns
+from tomlkit import document
+
+import pacs.common_vars as common_vars
+from pacs.manager.task_manager import TaskManager
+from pacs.manager.validation_manager import ValidationManager
+from pacs.utils import difference_list, parse_toml_file, toml_to_file
+
+service_state_file = common_vars.state_dir / "managed_service.toml"
+
+
+class ServiceManager:
+    def __init__(self, vm: ValidationManager):
+        self.services_to_enable: list[str] = []
+
+        # Get the list of all managed services
+        if not service_state_file.exists():
+            self.managed_services: list[str] = []
+        else:
+            service_state_data = parse_toml_file(service_state_file)
+            for key, value in service_state_data.items():
+                if key == "managed_services":
+                    self.managed_services: list[str] = value
+                else:
+                    vm.validate(
+                        False,
+                        f"There is unknown key in the state file for services: {key}",
+                    )
+
+        # Get all services that is available in the system
+        result = subprocess.run(
+            [
+                "systemctl",
+                "list-unit-files",
+                "--type=service",
+                "--no-pager",
+                "--no-legend",
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        vm.validate(
+            result.returncode == 0,
+            f"Cannot determine services available in system.\n{result.stderr.strip()}",
+        )
+
+        self.services_in_system = []
+
+        for line in result.stdout.strip().split("\n"):
+            if not line:
+                continue
+
+            parts = line.split()
+            if len(parts) >= 2:
+                name, state = parts[0], parts[1]
+
+                if state not in ["static", "indirect", "generated", "transient"]:
+                    self.services_in_system.append(name)
+
+    def add_services_to_enable(
+        self, service_name: str | list[str], vm: ValidationManager
+    ) -> None:
+        if isinstance(service_name, str):
+            service_name = [service_name]
+
+        for service in service_name:
+            vm.validate(
+                service in self.services_in_system,
+                f'Cannot find service "{service}" in the system.',
+            )
+
+            if service not in self.services_to_enable:
+                self.services_to_enable.append(service)
+
+    def enable_services(self, services: list[str], vm: ValidationManager) -> None:
+        for service in services:
+            result = subprocess.run(
+                ["systemctl", "enable", service],
+                capture_output=True,
+                text=True,
+            )
+
+            if vm.validate(result.returncode == 0, f'Enabling "{service}" failed'):
+                if service not in self.managed_services:
+                    self.managed_services.append(service)
+
+    def disable_services(self, services: list[str], vm: ValidationManager) -> None:
+        for service in services:
+            result = subprocess.run(
+                ["systemctl", "disable", service],
+                capture_output=True,
+                text=True,
+            )
+
+            if vm.validate(result.returncode == 0, f'Disabling "{service}" failed'):
+                if service in self.managed_services:
+                    self.managed_services.remove(service)
+
+    def update_service_state_file(self):
+        doc = document()
+        doc["managed_services"] = self.managed_services
+        toml_to_file(service_state_file, doc)
+
+    def execute(self, tm: TaskManager, vm: ValidationManager):
+        # Determine services to disable
+        # These are services that was enabled in the past but has been removed from config this time
+        services_to_disable = difference_list(
+            self.managed_services, self.services_to_enable
+        )
+
+        # Determine services that actually need enabling
+        services_to_enable = []
+
+        for service in self.services_to_enable:
+            result = subprocess.run(
+                ["systemctl", "is-enabled", service],
+                capture_output=True,
+                text=True,
+            )
+
+            if result.stdout.strip() != "enabled":
+                services_to_enable.append(service)
+
+        # Enable services
+        if services_to_enable:
+            services_to_enable.sort()
+
+            tm.add_post_task(
+                self.enable_services,
+                Columns(
+                    services_to_enable,
+                    title="Enable the following services",
+                ),
+                services_to_enable,
+                vm,
+            )
+
+        # Disable services
+        if services_to_disable:
+            services_to_disable.sort()
+
+            tm.add_pre_task(
+                self.disable_services,
+                Columns(
+                    services_to_disable,
+                    title="Disable the following services",
+                ),
+                services_to_disable,
+                vm,
+            )
+
+        # Update the service state file
+        if self.managed_services:
+            tm.add_task(
+                self.update_service_state_file,
+                "Update service state file",
+            )
